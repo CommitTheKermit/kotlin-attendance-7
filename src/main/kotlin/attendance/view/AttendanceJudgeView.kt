5750c681407c333c8e6523a27f5bd42b81ad909e
CommitTheKermit <qnf323@naver.com>
2026-01-01 20:20:27 +0900

    feat(AttendanceService): 크루별 출석 기록 확인 - 경고 판정

--- src/main/kotlin/attendance/view/AttendanceJudgeView.kt
+++ src/main/kotlin/attendance/view/AttendanceJudgeView.kt
@@ -11,10 +11,32 @@ object AttendanceJudgeView {
 
     fun showAttendance(attendances: List<Attendance>) {
         val formatter = DateTimeFormatter.ofPattern("MM월 dd일 EEEE HH:mm")
+
+        var attendCount = 0
+        var lateCount = 0
+        var absentCount = 0
+
         attendances.forEach {
             println("${it.dateTime.format(formatter)} (${it.status})")
 
+            when (it.status) {
+                AttStatus.ATTEND -> attendCount++
+                AttStatus.LATE -> lateCount++
+                AttStatus.ABSENT -> absentCount++
+            }
         }
-
+        println()
+        println(
+            """
+            출석: ${attendCount}회
+            지각: ${lateCount}회
+            결석: ${absentCount}회
+        """.trimIndent()
+        )
+        println()
+        if (absentCount + lateCount / 3 > 3) {
+            println("면담 대상자입니다.\n")
+        }
+        println()
     }
 }
