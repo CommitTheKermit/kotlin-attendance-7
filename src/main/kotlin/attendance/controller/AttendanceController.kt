32446f9d306923d81a1d33a9d0b512986dec7027
CommitTheKermit <qnf323@naver.com>
2026-01-01 19:19:47 +0900

    feat(AttendanceController): 가이드 출력

--- src/main/kotlin/attendance/controller/AttendanceController.kt
+++ src/main/kotlin/attendance/controller/AttendanceController.kt
@@ -5,6 +5,7 @@ import attendance.service.Parser
 import attendance.service.Validator
 import attendance.view.AttendanceConfirmView
 import attendance.view.InputView
+import attendance.view.OutputView
 import camp.nextstep.edu.missionutils.DateTimes
 import java.io.File
 import java.time.LocalDate
@@ -12,11 +13,14 @@ import java.time.LocalDateTime
 import java.time.LocalTime
 
 class AttendanceController {
-    val attendanceList : MutableList<Attendance> = fileRead()
+    val attendanceList: MutableList<Attendance> = fileRead()
     fun run() {
         while (true) {
+
+            OutputView.showFunctionGuide()
             val functionNumber = InputView.readLine()
             Validator.functionNumberValidate(functionNumber);
+            OutputView.showNewLine()
             when (functionNumber) {
                 "1" -> attendanceConfirm()
                 "2" -> {}
---
+++ src/main/kotlin/attendance/view/OutputView.kt
@@ -0,0 +1,25 @@
+package attendance.view
+
+import camp.nextstep.edu.missionutils.DateTimes
+import java.time.format.DateTimeFormatter
+
+object OutputView {
+    fun showFunctionGuide() {
+
+        val now = DateTimes.now()
+        println(
+            """
+                오늘은 ${now.format(DateTimeFormatter.ofPattern("MM월 dd일 EEEE"))}입니다. 기능을 선택해 주세요.
+                1. 출석 확인
+                2. 출석 수정
+                3. 크루별 출석 기록 확인
+                4. 제적 위험자 확인
+                Q. 종료
+            """.trimIndent()
+        )
+    }
+
+    fun showNewLine() {
+        println()
+    }
+}
